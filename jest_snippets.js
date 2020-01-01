eacf93713ee8affeb0a2dbe04bab4e71a0b0d6c0
Umesh Nikam <Umesh.Nikam@synechron.com>
2020-01-01 11:41:22 +0530

    Jest Cheat sheet is updated with MOCK and asynchronous calls

--- jest_snippets.js
+++ jest_snippets.js
@@ -233,3 +233,309 @@
     test('the fetch fails with an error', async () => {
       await expect(fetchData()).rejects.toThrow('error');
     });
+
+    //to be continue on https://jestjs.io/docs/en/setup-teardown
+    // "beforeEach and afterEach" -> executes after every tests runs
+    //                            -> beforeEach and afterEach can handle asynchronous code in the same ways that tests can handle asynchronous code - they can either take a done parameter or return a promise.
+    // "beforeAll and afterAll" -> executes once
+    beforeEach(() => {
+      initializeCityDatabase();
+    });
+
+    afterEach(() => {
+      clearCityDatabase();
+    });
+
+    test('city database has Vienna', () => {
+      expect(isCity('Vienna')).toBeTruthy();
+    });
+
+    test('city database has San Juan', () => {
+      expect(isCity('San Juan')).toBeTruthy();
+    });
+
+    //with Promise
+    beforeEach(() => {
+      return initializeCityDatabase();
+    });
+
+    //One-Time Setup
+    beforeAll(() => {
+      return initializeCityDatabase();
+    });
+
+    afterAll(() => {
+      return clearCityDatabase();
+    });
+
+    test('city database has Vienna', () => {
+      expect(isCity('Vienna')).toBeTruthy();
+    });
+
+    test('city database has San Juan', () => {
+      expect(isCity('San Juan')).toBeTruthy();
+    });
+
+    //Scoping
+    //By default, the before and after blocks apply to every test in a file.
+    //"describe" -> use to group tests together as a block.
+    // Applies to all tests in this file
+    beforeEach(() => {
+      return initializeCityDatabase();
+    });
+
+    test('city database has Vienna', () => {
+      expect(isCity('Vienna')).toBeTruthy();
+    });
+
+    test('city database has San Juan', () => {
+      expect(isCity('San Juan')).toBeTruthy();
+    });
+
+    describe('matching cities to foods', () => {
+      // Applies only to tests in this describe block
+      beforeEach(() => {
+        return initializeFoodDatabase();
+      });
+
+      test('Vienna <3 sausage', () => {
+        expect(isValidCityFoodPair('Vienna', 'Wiener Schnitzel')).toBe(true);
+      });
+
+      test('San Juan <3 plantains', () => {
+        expect(isValidCityFoodPair('San Juan', 'Mofongo')).toBe(true);
+      });
+    });
+
+    //execution priority.
+    //the top-level beforeEach is executed before the beforeEach inside the describe block.
+    beforeAll(() => console.log('1 - beforeAll'));
+    afterAll(() => console.log('1 - afterAll'));
+    beforeEach(() => console.log('1 - beforeEach'));
+    afterEach(() => console.log('1 - afterEach'));
+    test('', () => console.log('1 - test'));
+    describe('Scoped / Nested block', () => {
+        beforeAll(() => console.log('2 - beforeAll'));
+        afterAll(() => console.log('2 - afterAll'));
+        beforeEach(() => console.log('2 - beforeEach'));
+        afterEach(() => console.log('2 - afterEach'));
+        test('', () => console.log('2 - test'));
+    });
+
+    // 1 - beforeAll
+    // 1 - beforeEach
+    // 1 - test
+    // 1 - afterEach
+    // 2 - beforeAll
+    // 1 - beforeEach
+    // 2 - beforeEach
+    // 2 - test
+    // 2 - afterEach
+    // 1 - afterEach
+    // 2 - afterAll
+    // 1 - afterAll
+
+    //Order of execution of describe and test blocks
+    describe('outer', () => {
+      console.log('describe outer-a');
+
+      describe('describe inner 1', () => {
+        console.log('describe inner 1');
+        test('test 1', () => {
+          console.log('test for describe inner 1');
+          expect(true).toEqual(true);
+        });
+      });
+
+      console.log('describe outer-b');
+
+      test('test 1', () => {
+        console.log('test for describe outer');
+        expect(true).toEqual(true);
+      });
+
+      describe('describe inner 2', () => {
+        console.log('describe inner 2');
+        test('test for describe inner 2', () => {
+          console.log('test for describe inner 2');
+          expect(false).toEqual(false);
+        });
+      });
+
+      console.log('describe outer-c');
+    });
+
+    // describe outer-a
+    // describe inner 1
+    // describe outer-b
+    // describe inner 2
+    // describe outer-c
+    // test for describe inner 1
+    // test for describe outer
+    // test for describe inner 2
+    // "test.only" -> this will be the only test that runs
+    test.only('this will be the only test that runs', () => {
+      expect(true).toBe(false);
+    });
+
+    test('this test will not run', () => {
+      expect('A').toBe('A');
+    });//this wont be executed...
+
+
+//Mock Functions
+    //Mock functions allow you to test the links between code by erasing the actual implementation of a function, capturing calls to the function (and the parameters passed in those calls), capturing instances of constructor functions when instantiated with new, and allowing test-time configuration of return values.
+    //two ways
+        //1.by creating a mock function to use in test code
+        //2.writing a manual mock to override a module dependency
+    //Using a mock function -> consider forEach function test case
+    function forEach(items, callback) {
+      for (let index = 0; index < items.length; index++) {
+        callback(items[index]);
+      }
+    }
+
+    const mockCallback = jest.fn(x => 42 + x);
+    forEach([0, 1], mockCallback);
+
+    // The mock function is called twice(i.e. array length)
+    expect(mockCallback.mock.calls.length).toBe(2);
+
+    // The first argument of the first call to the function was 0
+    expect(mockCallback.mock.calls[0][0]).toBe(0);
+
+    // The first argument of the second call to the function was 1
+    expect(mockCallback.mock.calls[1][0]).toBe(1);
+
+    // The return value of the first call to the function was 42
+    expect(mockCallback.mock.results[0].value).toBe(42);
+
+    //.mock property
+        //tells about how the function has been called and what the function returned is kept
+        //tracks the value of this for each call, so it is possible to inspect this as well
+
+    const myMock = jest.fn();
+
+    const a = new myMock();
+    const b = {};
+    const bound = myMock.bind(b);
+    bound();
+
+    console.log(myMock.mock.instances);
+    // > [ <a>, <b> ]
+
+    // The function was called exactly once
+    expect(someMockFunction.mock.calls.length).toBe(1);
+
+    // The first arg of the first call to the function was 'first arg'
+    expect(someMockFunction.mock.calls[0][0]).toBe('first arg');
+
+    // The second arg of the first call to the function was 'second arg'
+    expect(someMockFunction.mock.calls[0][1]).toBe('second arg');
+
+    // The return value of the first call to the function was 'return value'
+    expect(someMockFunction.mock.results[0].value).toBe('return value');
+
+    // This function was instantiated exactly twice
+    expect(someMockFunction.mock.instances.length).toBe(2);
+
+    // The object returned by the first instantiation of this function
+    // had a `name` property whose value was set to 'test'
+    expect(someMockFunction.mock.instances[0].name).toEqual('test');
+
+    //Mock Return Values
+    //Mock functions can also be used to inject test values into your code during a test
+    const myMock = jest.fn();
+    console.log(myMock());
+    // > undefined
+
+    myMock
+      .mockReturnValueOnce(10)
+      .mockReturnValueOnce('x')
+      .mockReturnValue(true);
+
+    console.log(myMock(), myMock(), myMock(), myMock());
+    // > 10, 'x', true, true
+
+    //Mock functions are also very effective in code that uses a functional continuation-passing style.
+    const filterTestFn = jest.fn();
+
+    // Make the mock return `true` for the first call,
+    // and `false` for the second call
+    filterTestFn.mockReturnValueOnce(true).mockReturnValueOnce(false);
+
+    const result = [11, 12].filter(num => filterTestFn(num));
+
+    console.log(result);
+    // > [11]
+    console.log(filterTestFn.mock.calls);
+    // > [ [11], [12] ]
+
+//Mocking Modules
+    // users.js
+    import axios from 'axios';
+
+    class Users {
+      static all() {
+        return axios.get('/users.json').then(resp => resp.data);
+      }
+    }
+
+    export default Users;
+    //create jest.mock(...) function to automatically mock the axios module w/o hitting actual API
+
+    // users.test.js
+    import axios from 'axios';
+    import Users from './users';
+
+    jest.mock('axios');
+
+    test('should fetch users', () => {
+        const users = [{name: 'Bob'}];
+        const resp = {data: users};
+        axios.get.mockResolvedValue(resp);
+
+        // or you could use the following depending on your use case:
+        // axios.get.mockImplementation(() => Promise.resolve(resp))
+
+        return Users.all().then(data => expect(data).toEqual(users));
+    });
+
+//Mock Implementations
+    //"mockImplementation" -> method is useful when you need to define the default implementation of a mock function that is created from another module
+
+    // foo.js
+    module.exports = function() {
+      // some implementation;
+    };
+
+    // test.js
+    jest.mock('../foo'); // this happens automatically with automocking
+    const foo = require('../foo');
+
+    // foo is a mock function
+    foo.mockImplementation(() => 42);
+    foo();
+    // > 42
+
+    //"mockImplementationOnce" -> used When you need to recreate a complex behavior of a mock function such that multiple function calls produce different results
+    const myMockFn = jest
+      .fn()
+      .mockImplementationOnce(cb => cb(null, true))
+      .mockImplementationOnce(cb => cb(null, false));
+
+    myMockFn((err, val) => console.log(val));
+    // > true
+
+    myMockFn((err, val) => console.log(val));
+    // > false
+
+    //Or
+
+    const myMockFn = jest
+      .fn(() => 'default')
+      .mockImplementationOnce(() => 'first call')
+      .mockImplementationOnce(() => 'second call');
+
+    console.log(myMockFn(), myMockFn(), myMockFn(), myMockFn());
+    // > 'first call', 'second call', 'default', 'default
